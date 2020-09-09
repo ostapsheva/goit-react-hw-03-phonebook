ab6cf6ea1b632e017202bfe1e293bb725d3439b0
Ostap Shevchenko <ostapshev@gmail.com>
2020-09-10 00:25:22 +0300

    Updates

---
+++ precache-manifest.73efb070906ba8dd79845749c47b28c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5868afc6db66cfe3a55cc21327d6299",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "39ae552db5ec0f3080ab",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "9bfbfbcabfceb5122491",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.0d631485.chunk.css"
+  },
+  {
+    "revision": "39ae552db5ec0f3080ab",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.67db1eed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.67db1eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfbfbcabfceb5122491",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.0a2807af.chunk.js"
+  },
+  {
+    "revision": "8fccd0a0639088505f9b",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.4789c2a7.js"
+  }
+]);
