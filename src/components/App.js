7a639dd2693f6f3ed227dd5204bca02a790aec87
Ostap Shevchenko <ostapshev@gmail.com>
2020-09-10 01:59:39 +0300

    add task 03

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,24 @@ import styles from './App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleChange = event => {
     this.setState({ name: event.target.value });
@@ -51,9 +61,11 @@ class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm onAddContact={this.addContact}></ContactForm>
         <h2>Contacts</h2>
+        {contacts.length === 0 ? <p>No contacts...</p> : null}
         {contacts.length > 1 ? (
           <Filter value={filter} onChangeFilter={this.onChangeFilter} />
         ) : null}
+
         <ContactsList
           contacts={filteredContacts}
           onRemoveContact={this.removeContact}
